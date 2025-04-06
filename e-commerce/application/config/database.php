6354fef56b0eb5737e20988b12afede6eec83a1f
riskiegenerous27 <55268828+riskiegenerous27@users.noreply.github.com>
2025-04-06 23:05:31 +0700

    Add e-commerce/application/config/database.php 1743955530575

---
+++ e-commerce/application/config/database.php
@@ -0,0 +1,25 @@
+<?php
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'   => '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'e_commerce',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
